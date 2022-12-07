3dfe4086904f2d04570aced47642663255ddb631
yang20002020 <2628636987@qq.com>
2022-12-08 01:10:17 +0800

    操作数据库redis测试案例

---
+++ bj38web_055/web/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bj38web_055/web/controller"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//gin框架开发三步骤
+	//1 初始化路由
+	router := gin.Default()
+	//2 路由匹配
+	//router.GET("/", func(context *gin.Context) {
+	//	context.Writer.WriteString("项目开始了........")
+	//})
+	router.Static("/home", "view") //加载页面
+	router.GET("/api/v1.0/session", controller.GetSession)
+	//
+	fmt.Println("*********000000000000*******")
+	router.GET("/api/v1.0/imagecode/:uuid", controller.GetImageCd)
+	//3启动运行
+	router.Run()
+}
