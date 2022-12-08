eba81fefd9f44decfd1e8f80f8ece2122ed72d93
yang20002020 <2628636987@qq.com>
2022-12-08 23:26:41 +0800

    从session中获取用户名，修改Getsession函数

---
+++ bj38web_071/web/controller/user.go
@@ -0,0 +1,338 @@
+package controller
+
+import (
+	"bj38web_071/web/model"
+	"bj38web_071/web/proto/getCaptcha"
+	userMico "bj38web_071/web/proto/user"
+	"bj38web_071/web/utils"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/afocus/captcha"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	dysmsapi20170525 "github.com/alibabacloud-go/dysmsapi-20170525/v3/client"
+	util "github.com/alibabacloud-go/tea-utils/v2/service"
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/asim/go-micro/plugins/registry/consul/v3"
+	"github.com/asim/go-micro/v3"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/gomodule/redigo/redis"
+	"image/png"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+// 根据gin框架和文档业务要求写函数内容
+// 获取session信息
+func GetSession(ctx *gin.Context) {
+	// 初始化错误返回的map
+	//resp := make(map[string]string)
+	//resp["errno"] = utils.RECODE_SESSIONERR
+	//resp["errmsg"] = utils.RecodeText(utils.RECODE_SESSIONERR)
+	fmt.Println("*************GetSession 函数************")
+	resp := make(map[string]interface{})
+	//获取session数据
+	s := sessions.Default(ctx) //session初始化
+	//func PostLogin(ctx *gin.Context)  函数中已经定义了userName名称
+	userName := s.Get("userName") //获取手机号
+	//用户没有登录 ----- 没有存在mysql中，也没有存在session 中
+	if userName == nil {
+		resp["errno"] = utils.RECODE_SESSIONERR
+		resp["errmsg"] = utils.RecodeText(utils.RECODE_SESSIONERR)
+		fmt.Println("用户没有登录****")
+	} else {
+		resp["errno"] = utils.RECODE_OK
+		resp["errmsg"] = utils.RecodeText(utils.RECODE_OK)
+		/*{"data":{"name":"15889**********"},"errmsg":"成功","errno":"0"}*/
+		var nameData struct {
+			Name string `json:"name"`
+		}
+		nameData.Name = userName.(string) //类型断言
+		resp["data"] = nameData
+		fmt.Println("用户已经登录****")
+	}
+
+	//根军gin框架要求 按照json格式进行发送数据
+	ctx.JSON(http.StatusOK, resp)
+}
+
+// 获取图片信息
+func GetImageCd(ctx *gin.Context) {
+	fmt.Println("*********1111111111*******")
+	//获取图片验证码 uuid
+	uuid := ctx.Param("uuid")
+	fmt.Println("uuid=", uuid)
+	/*************************/
+	consulReg := consul.NewRegistry()
+	consulService := micro.NewService(
+		micro.Registry(consulReg),
+	)
+	//调用 函数 .初始化客户端
+	microClient := getCaptcha.NewGetCaptchaService("go.micro.srv.getCaptcha", consulService.Client())
+	//调用远程函数
+	resp, err := microClient.Call(context.TODO(), &getCaptcha.Request{Uuid: uuid})
+	if err != nil {
+		fmt.Println("未能找到远程服务:", err)
+		return
+	}
+	//将得到的数据 反序列化，得到图片数据
+	var img captcha.Image
+	json.Unmarshal(resp.Msg, &img)
+	//对数据进行编码  将图片显示到浏览器
+	png.Encode(ctx.Writer, img)
+}
+
+// 获取短信验证码
+func GetSmscd(ctx *gin.Context) {
+
+	//获取短信验证码
+	phone := ctx.Param("phone")
+	//http://192.168.1.112:8080/api/v1.0/smscode/1588******?text=enhe&id=56b89c6b-d62d-45c2-b84d-e9d80d8f187c
+	//拆分GET请求中的URL ===格式： 资源路径？/k=v & k=v & k =v
+	//参考gin框架 中文文档
+	imgCode := ctx.Query("text")
+	uuid := ctx.Query("id")
+	fmt.Println("---OUT----:", phone, imgCode, uuid)
+	/*************需要转移到微服务的代码****************/
+	//resp := make(map[string]string)
+	////校验图片验证码 是否正确
+	//result := model.CheckImgCode(uuid, imgCode)
+	//if result {
+	//	//校验成功
+	//	fmt.Println("校验成功！！！")
+	//	//发送短信
+	//	err := _main(tea.StringSlice(os.Args[1:]), phone, resp)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//} else {
+	//	//校验失败 显示错误信息
+	//	resp["errno"] = utils.RECODE_DATAERR
+	//	resp["errmsg"] = utils.RecodeText(utils.RECODE_DATAERR)
+	//	fmt.Println("校验失败！！！")
+	//}
+	/******************************************/
+	/************微服务添加内容*********/
+	//指定consul服务发现
+	consulReg := consul.NewRegistry()
+	consulService := micro.NewService(
+		micro.Registry(consulReg),
+	)
+	//1.初始化客户端
+	microClient := userMico.NewUserService("go.micro.srv.user", consulService.Client())
+
+	//调用远程函数
+	resp, err := microClient.SendSms(context.TODO(), &userMico.Request{Phone: phone, ImgCode: imgCode, Uuid: uuid})
+	if err != nil {
+		fmt.Println("调用远程函数SendSms失败:", err)
+		return
+	}
+	/*********************/
+	//发送成功或者失败结果 给浏览器
+	ctx.JSON(http.StatusOK, resp)
+
+}
+
+func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *dysmsapi20170525.Client, _err error) {
+	config := &openapi.Config{
+		// 您的 AccessKey ID
+		AccessKeyId: accessKeyId,
+		// 您的 AccessKey Secret
+		AccessKeySecret: accessKeySecret,
+	}
+	// 访问的域名
+	config.Endpoint = tea.String("dysmsapi.aliyuncs.com")
+	_result = &dysmsapi20170525.Client{}
+	_result, _err = dysmsapi20170525.NewClient(config)
+	return _result, _err
+}
+
+func _main(args []*string, phone string, resp map[string]string) (_err error) {
+	client, _err := CreateClient(tea.String("***********"), tea.String("*********************"))
+	if _err != nil {
+		return _err
+	}
+	//随机生成一个验证码
+	rand.Seed(time.Now().UnixNano())
+	//生成六位随机数
+	str := rand.Int31n(1000000)
+	smsCode := fmt.Sprintf("%06d", str)
+	subStr := "{\"code\"" + ":" + "\"" + smsCode + "\"" + "}"
+	fmt.Println(subStr)
+	sendSmsRequest := &dysmsapi20170525.SendSmsRequest{
+		SignName:      tea.String("阿里云短信测试"),
+		TemplateCode:  tea.String("SMS_15**********"),
+		PhoneNumbers:  tea.String(phone), //158*************
+		TemplateParam: tea.String(subStr),
+	}
+	runtime := &util.RuntimeOptions{}
+	tryErr := func() (_e error) {
+		defer func() {
+			if r := tea.Recover(recover()); r != nil {
+				_e = r
+			}
+		}()
+
+		// 复制代码运行请自行打印 API 的返回值
+		//20221007 发送短信验证码
+
+		_, _err = client.SendSmsWithOptions(sendSmsRequest, runtime)
+		if _err != nil {
+			//20221007  发送验证码失败
+			fmt.Println("发送验证码失败！！！")
+			resp["errno"] = utils.RECODE_SMSERR
+			resp["errmsg"] = utils.RecodeText(utils.RECODE_SMSERR)
+			return _err
+		} else {
+			//20221007  发送验证码成功
+			resp["errno"] = utils.RECODE_OK
+			resp["errmsg"] = utils.RecodeText(utils.RECODE_OK)
+			fmt.Println("发送验证码成功！！！")
+			//将短信验证码，存入到redis 数据库
+			err := model.SaveSmsCode(phone, smsCode)
+			if err != nil {
+				fmt.Println("存储短信验证码到redis失败：", err)
+				resp["errno"] = utils.RECODE_DBERR
+				resp["errmsg"] = utils.RecodeText(utils.RECODE_DBERR)
+			}
+		}
+
+		return nil
+	}()
+
+	if tryErr != nil {
+		var error = &tea.SDKError{}
+		if _t, ok := tryErr.(*tea.SDKError); ok {
+			error = _t
+		} else {
+			error.Message = tea.String(tryErr.Error())
+		}
+		// 如有需要，请打印 error
+		_, _err = util.AssertAsString(error.Message)
+		if _err != nil {
+			return _err
+		}
+	}
+	return _err
+}
+
+// 发送注册信息
+/*{mobile: "158****", password: "111", sms_code: "94851"}
+mobile: "158******"
+password: "111"
+sms_code: "94851" */
+func PostRet(ctx *gin.Context) {
+
+	//获取数据
+	var regData struct {
+		Mobile   string "json:\"mobile\""
+		PassWord string "json:\"password\""
+		SmsCod   string "json:\"sms_code\""
+	}
+	ctx.Bind(&regData)
+	fmt.Println("mobile=", regData.Mobile, "pwd=", regData.PassWord, "sms_code=", regData.SmsCod)
+
+	//初始化consul
+	microService := utils.InitMicro()
+	microClient := userMico.NewUserService("go.micro.srv.user", microService.Client())
+	//调用远程函数
+	resp, err := microClient.Register(context.TODO(), &userMico.RegReq{
+		Mobile:   regData.Mobile,
+		SmsCode:  regData.SmsCod,
+		Password: regData.PassWord,
+	})
+	if err != nil {
+		fmt.Println("注册用户，找不到远程服务！", err)
+		return
+	}
+	//写给浏览器
+	ctx.JSON(http.StatusOK, resp)
+}
+
+// 获取地址信息的信息
+func GetArea(ctx *gin.Context) {
+	var areas []model.Area
+	//从缓存中获取数据
+	conn := model.RedisPool.Get()
+	//areaData字节切片
+	areaData, _ := redis.Bytes(conn.Do("get", "areaData"))
+	if len(areaData) == 0 {
+		//没有从redis中获取数据
+
+		fmt.Println("从mysql中获取数据 地址信息")
+		//先从mysql中获取数据
+		model.GlobalConn.Find(&areas)
+		//把数据写入到redis时，存储结构体序列化后json串
+		//字节切片areaBuf
+		areaBuf, _ := json.Marshal(areas)
+
+		conn := model.RedisPool.Get() //获取链接
+		//再把数据写入到reidis中， 以字节流的方式
+		conn.Do("set", "areaData", areaBuf)
+	} else {
+		fmt.Println("从redis中获取数据 地址信息")
+		//redis中有数据，取出数据
+		json.Unmarshal(areaData, &areas)
+	}
+	resp := make(map[string]interface{})
+	resp["errno"] = "0"
+	resp["errmsg"] = utils.RecodeText(utils.RECODE_OK)
+	resp["data"] = areas
+	ctx.JSON(http.StatusOK, resp)
+	fmt.Println("areas:", areas)
+	fmt.Println("************************111111111")
+}
+
+// 处理登录业务
+func PostLogin(ctx *gin.Context) {
+	//获取前端数据
+	var loginData struct {
+		Mobile   string `json:"mobile"`
+		PassWord string `json:"password"`
+	}
+	//获取前端数据
+	ctx.Bind(&loginData)
+	fmt.Println("获取登录数据：")
+	fmt.Println("mobile；", loginData.Mobile, "password:", loginData.PassWord)
+	resp := make(map[string]interface{})
+	//获取数据库数据 查询登录输入的数据是否和数据库的数据匹配
+	userName, err := model.Login(loginData.Mobile, loginData.PassWord)
+	if err == nil {
+		//登录成功
+		resp["errno"] = utils.RECODE_OK
+		resp["errmsg"] = utils.RecodeText(utils.RECODE_OK)
+		//1.初始化容器，在main函数中操作
+		//将登录状态 保存到Session中
+		s := sessions.Default(ctx)  //初始化session
+		s.Set("userName", userName) //将用户名设置到session中
+		s.Save()
+		fmt.Println("登录成功，session存储了userName")
+	} else {
+		//登录失败
+		resp["errno"] = utils.RECODE_LOGINERR
+		resp["errmsg"] = utils.RecodeText(utils.RECODE_LOGINERR)
+
+	}
+	ctx.JSON(http.StatusOK, resp)
+}
+
+// 退出登录
+func DeleteSession(ctx *gin.Context) {
+	resp := make(map[string]interface{})
+	//初始化 session对象
+	s := sessions.Default(ctx)
+	//删除session数据 没有返回值
+	s.Delete("userName")
+	err := s.Save() // 有返回值
+	if err != nil {
+		resp["errno"] = utils.RECODE_IOERR
+		resp["errmsg"] = utils.RecodeText(utils.RECODE_IOERR)
+	} else {
+		resp["errno"] = utils.RECODE_OK
+		resp["errmsg"] = utils.RecodeText(utils.RECODE_OK)
+	}
+	fmt.Println("退出登录***")
+	ctx.JSON(http.StatusOK, resp)
+}
---
+++ bj38web_071/web/model/modelFunc.go
@@ -0,0 +1,72 @@
+package model
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+)
+
+// 创建全局redis 连接池 句柄
+var RedisPool redis.Pool
+
+// 校验图片验证码
+func CheckImgCode(uuid, imgCode string) bool {
+	//链接数据库
+	//conn, err := redis.Dial("tcp", "192.168.1.112:6379")
+	//if err != nil {
+	//	fmt.Println("redis.Dial err:", err)
+	//	return false
+	//}
+	// 从连接池中获取链接
+	conn := RedisPool.Get()
+	defer conn.Close()
+
+	//查询redis 数据
+	//Do(commandName string, args ...interface{}) (reply interface{}, err error)
+	code, err := redis.String(conn.Do("get", uuid))
+	if err != nil {
+		fmt.Println("查询错误  err:", err)
+		return false
+	}
+	//返回校验结果
+	return code == imgCode
+}
+
+// 创建函数  初始化连接池
+func InitRedis() {
+	RedisPool = redis.Pool{
+		MaxIdle:         20,
+		MaxActive:       50,
+		MaxConnLifetime: 60 * 5,
+		IdleTimeout:     60, //超过60秒就会被断掉数据库
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "192.168.1.112:6379")
+		},
+	}
+}
+
+// 存储短信验证码
+func SaveSmsCode(phone, code string) error {
+	//链接redis 连接池 从连接池中获取链接
+	conn := RedisPool.Get()
+	defer conn.Close()
+
+	//存储图片验证码 到redis中
+	_, err := conn.Do("setex", phone+"_code", 60*3, code)
+	return err
+}
+
+// 处理登录业务 根据手机号和密码获取用户名
+func Login(mobile, pwd string) (string, error) {
+	var user User
+	//对参数pwd 做md5 hash
+	m5 := md5.New()
+	m5.Write([]byte(pwd))
+	pwd_hash := hex.EncodeToString(m5.Sum(nil))
+	//mobile name  都是mysql指定的字段
+	//根据 函数参数 mobile 和pwd 查找用户名
+	// select name from user where  mobile=** and pwd_hash=***
+	err := GlobalConn.Where("mobile=?", mobile).Select("name").Where("password_hash=?", pwd_hash).Find(&user).Error
+	return user.Name, err
+}
